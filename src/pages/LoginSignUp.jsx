29d98fc09bf4758faf478ee596da4a03e3e2284b
Mridul Shukla <75447397+mrisham@users.noreply.github.com>
2024-01-28 21:49:14 +0530

    designed login/signup

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import "./css/loginsignup.css";
 const LoginSignUp = () => {
-  return <div>LoginSignUp</div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-feilds">
+          <input type="text" placeholder="name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          <span>Already have an account? </span>Login here
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" />
+          <p>By continuing, I agree to terms of use and privacy policies</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignUp;
--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -33,6 +33,7 @@ const ShopCategory = (props) => {
           }
         })}
       </div>
+      <div className="shopcategory-loadmore">Explore More</div>
     </div>
   );
 };
